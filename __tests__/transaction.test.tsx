8939064620f3da01fc00b2c36c4633112ce3568b
Colt45s <kzk0693@gmail.com>
2019-03-06 21:55:52 +0900

    fix(test): type

--- __tests__/transaction.test.tsx
+++ __tests__/transaction.test.tsx
@@ -3,18 +3,19 @@ import { render, cleanup } from 'react-testing-library'
 import 'jest-dom/extend-expect'
 
 import Transaction from '../src/components/transaction'
+import { Row } from '../src/components/useTransactions'
 
 afterEach(cleanup)
 
 test('snapshot props', () => {
-  const props = {
+  const props: { transaction: Row } = {
     transaction: {
       player: 'test',
       team: 'hou',
       note: 'test',
       from_team: 'astros',
       type: '',
-      type_cd: ''
+      type_cd: 'RET'
     }
   }
 
