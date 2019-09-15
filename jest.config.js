3381de74b18018c965e2b50383a55868c8a5ed5d
Colt45s <kzk0693@gmail.com>
2019-09-16 01:20:26 +0900

    chore(test): update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  preset: 'ts-jest',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'jest.tsconfig.json'
+    }
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
